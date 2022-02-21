2f5766133b9222eb811285946f1d89983b034a4f
NizHiz <100023961+NizHiz@users.noreply.github.com>
2022-02-21 23:02:42 +0700

    Create Exercise4_N_H.py

---
+++ Exercise4_N_H.py
@@ -0,0 +1,9 @@
+FE_Score = 90.1
+GB_Score = 90.2
+IC_Sore = 90.3
+CP_Score = 90.4
+print('--- Your Score ---')
+print('Foundation English :', FE_Score)
+print('General Business :', GB_Score)
+print('Introduction to Computer Systems :', IC_Sore)
+print('Computer Programming :', CP_Score)
