71f455d2560f16da74480b5dd848ee4fa7242b32
NizHiz <100023961+NizHiz@users.noreply.github.com>
2022-02-22 00:54:50 +0700

    Create Exercise5.1_N_H.py

---
+++ Exercise5.1_N_H.py
@@ -0,0 +1,10 @@
+x = int(input('number 1 : '))
+y = int(input('number 2 : '))
+z = x + y
+i = x - y
+j = x * y
+h = x / y
+print(x, '+', y, '=', z)
+print(x, '-', y, '=', i)
+print(x, '*', y, '=', j)
+print(x, '/', y, '=', h)
