7436f76e87636d59c9e21307f6b78e80bd635c13
NizHiz <100023961+NizHiz@users.noreply.github.com>
2022-02-23 17:55:55 +0700

    Create Exercise9_N_H.py

---
+++ Exercise9_N_H.py
@@ -0,0 +1,6 @@
+username = input('username : ')
+password = input('password : ')
+while username != 'abc' or password != '123':
+    username = input('username : ')
+    password = input('password : ')
+print('Done !')
